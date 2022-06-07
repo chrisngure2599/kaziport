585670d758b6e9be6a749d9c6226d984239c3bc5
Chris Ngure <chrisngure2599@gmail.com>
2022-06-07 11:19:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var scraper=require('./scraper');
-var time=600;//This times milliseconds 1000 = 1 second
-(function loop() {
+var time=600//This times milliseconds 1000 = 1 second
+function loop() {
   setTimeout(function () {
-    scraper.scrape();
+    // scraper.scrape();
     console.log('Scraping....')
     loop()
   }, 1000*time); //9000 = 9000ms = 9s
-});
+}
+loop();
